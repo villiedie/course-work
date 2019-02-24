e4cdcafaefb7b1ca577b55eba1b4944ceb88d0f0
villiedie <43264799+villiedie@users.noreply.github.com>
2019-02-24 21:47:39 +0300

    First week's subtask
    Learning how to use pymorphy2

---
+++ pym2.py
@@ -0,0 +1,34 @@
+import pymorphy2
+morph = pymorphy2.MorphAnalyzer()
+inlo = input("Введите слово:")
+p = morph.parse(inlo)[0]
+if p.tag.POS == 'NOUN' or p.tag.POS == 'ADJF' or p.tag.POS == 'PRTF' or p.tag.POS == 'NUMR' or p.tag.POS == 'NPRO':
+    print("Падеж слова ")
+    if p.tag.case == 'nomn':
+        print("Именительный")
+    elif p.tag.case == 'gent':
+        print("Родительный")
+    elif p.tag.case == 'datv':
+        print("Дательный")
+    elif p.tag.case == 'accs':
+        print("Винительный")
+    elif p.tag.case == 'ablt':
+        print("Творительный")
+    elif p.tag.case == 'loct':
+        print("Предложный")
+    elif p.tag.case == 'voct':
+        print("Звательный")
+    elif p.tag.case == 'gen2':
+        print("Второй родительный(частичный)")
+    elif p.tag.case == 'acc2':
+        print("Второй винительный")
+    elif p.tag.case == 'loc2':
+        print("Второй предложный(местный)")
+if p.tag.number == 'sing':
+    print("Единственное число")
+else:
+    print("Множественное число")
+print("Нормальная форма слова: ", p.normal_form)
+
+
+
